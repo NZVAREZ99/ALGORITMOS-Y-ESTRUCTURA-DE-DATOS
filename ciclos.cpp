9bdff4d1e99e73272211c18f22b5f7dd956aee1a
Nicolas Alvarez <nzvarez833@gmail.com>
2022-12-06 21:10:08 -0300

    mostrando numeros con ciclo while y for

---
+++ ciclos.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	int i;
+	printf ("Imprimiendo numeros de 1 a 10= \n");
+	for (i=1;i<=10;i++)
+	{
+	     printf ("%d\n",i);
+	}
+	
+	printf ("Imprimiendo numeros de 11 a 20= \n");
+	
+	int j=11;
+	while  (j<=20)
+	{
+	     printf ("%d\n",j);
+	     j++;
+	}
+	
+}
